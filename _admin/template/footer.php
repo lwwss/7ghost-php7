cbae8f6614617427fa1a061ea2f7460e38b3571f
lwwss <lwwss@users.noreply.github.com>
2020-02-14 19:01:21 +0800

    Add files via upload

---
+++ _admin/template/footer.php
@@ -0,0 +1,8 @@
+	<div class="bottom">
+		<div id="copyright">	
+		&copy;2019 7ghost v1.1 utf8 <a href='http://www.7ghost.cn/'>新版下载</a>  
+		</div>
+	</div>
+</div><!-- end .doc -->
+</body>
+</html>
---
+++ _admin/template/login.php
@@ -0,0 +1,15 @@
+<form action="" method="post">
+<table cellspacing="0">
+<tbody>
+<!--
+<tr><th>用户名</th><td><input name="username" value="">
+</td></tr>
+-->
+<tr><th>密码：</th><td><input type="password" name="password"><input type="submit" value="登录">
+</td></tr>
+</tbody></table>
+	<!--
+<p><input type="submit" value="登录">
+</p><div></div>
+-->
+</form>
